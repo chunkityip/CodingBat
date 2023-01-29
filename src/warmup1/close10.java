f95d01b18aafce83067ffd42e3c8fa58f5a9b2d4
chunkityip <alex0917lfo@hotmail.com>
2023-01-29 16:13:48 +0800

    "Adding multiple classes in 1/29 in HK"

---
+++ src/warmup1/close10.java
@@ -0,0 +1,26 @@
+package warmup1;
+
+public class close10 {
+    /*
+    Given 2 int values, return whichever value is
+    nearest to the value 10, or return 0 in the event of a tie.
+    Note that Math.abs(n) returns the absolute value of a number.
+
+        close10(8, 13) → 8
+        close10(13, 8) → 8
+        close10(13, 7) → 0
+
+     */
+    public static void main (String[] args) {
+        System.out.println(close10(8, 13));
+        System.out.println(close10(13, 8));
+        System.out.println(close10(13, 7));
+    }
+
+    public static int close10(int a , int b) {
+        int aClose = Math.abs(a - 10);
+        int bClose = Math.abs(b - 10);
+        if (aClose == bClose) return 0;
+        return aClose < bClose ? a : b;
+    }
+}
