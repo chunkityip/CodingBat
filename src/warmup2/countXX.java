0d4e429a0d26e6c98e2c050788f305d5cc272cb8
chunkityip <alex0917lfo@hotmail.com>
2023-01-31 16:42:13 +0800

    "Adding countXX class in 1/31 in HK"

---
+++ src/warmup2/countXX.java
@@ -0,0 +1,28 @@
+package warmup2;
+
+public class countXX {
+    /*
+    Count the number of "xx" in the given string.
+    we'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+       countXX("abcxx") → 1
+       countXX("xxx") → 2
+       countXX("xxxx") → 3
+     */
+    public static void main (String[] args) {
+        System.out.println(countXX("abcxx"));
+        System.out.println(countXX("xxx"));
+        System.out.println(countXX("xxxx"));
+    }
+
+    public static int countXX(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.substring(i , i + 2).equals("xx")) {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+}
