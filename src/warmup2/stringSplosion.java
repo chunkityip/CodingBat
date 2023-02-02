353f29d5b1145d6a71e6a33a931c7fa2b0d4fb00
chunkityip <alex0917lfo@hotmail.com>
2023-02-02 16:02:50 +0800

    "Adding multiple classes in 2/2 in HK"

---
+++ src/warmup2/stringSplosion.java
@@ -0,0 +1,36 @@
+package warmup2;
+
+public class stringSplosion {
+    /*
+
+        Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+        stringSplosion("Code") → "CCoCodCode"
+        stringSplosion("abc") → "aababc"
+        stringSplosion("ab") → "aab"
+     */
+    public static void main (String[] args) {
+        System.out.println(stringSplosion("Code"));
+        System.out.println(stringSplosion("abc"));
+        System.out.println(stringSplosion("ab"));
+    }
+
+    public static String stringSplosion(String str) {
+  /*
+  Here is the pattern
+  C + Co + Cod + Code
+  It means it will increase a string following by the loop of the i as (i . i + 1)
+  C as (0 ,1)
+  Co as (0 , 2)
+  Cod as (0 , 3)
+  Code as (0 , 4)
+  so basically we just need to use the loop of i and substring() methid
+  */
+        String ans = "";
+        for (int i = 0; i < str.length(); i++) {
+            ans += str.substring(0 , i + 1);
+        }
+        return ans;
+    }
+
+}
