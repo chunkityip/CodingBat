0eacf481a6ad31bc28d485feacb70bdcef7b9040
chunkityip <alex0917lfo@hotmail.com>
2023-02-11 02:23:27 -0500

    Adding array667 class in HK on 2/10

--- src/warmup2/stringYak.java
+++ src/warmup2/stringYak.java
@@ -1,6 +1,6 @@
 package warmup2;
 
-public class stringYek {
+public class stringYak {
     /*
     Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed, but the "a" can be any char. The "yak" strings will not overlap.
 
