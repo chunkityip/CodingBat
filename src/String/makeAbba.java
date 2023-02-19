7797ffcc5185bb7f0101ebcebc0f7dcb4b76284a
chunkityip <alex0917lfo@hotmail.com>
2023-02-19 15:23:40 +0800

    Adding multiple classes in String

---
+++ src/String/makeAbba.java
@@ -0,0 +1,23 @@
+package String;
+
+public class makeAbba {
+    /*
+    Given two strings, a and b, return the result of putting them
+    together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+
+    makeAbba("Hi", "Bye") → "HiByeByeHi"
+    makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+    makeAbba("What", "Up") → "WhatUpUpWhat"
+
+     */
+    public static void main(String[] args) {
+        System.out.println(makeAbba("Hi", "Bye"));
+        System.out.println(makeAbba("Yo", "Alice"));
+        System.out.println(makeAbba("What", "Up"));
+    }
+
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
+}
