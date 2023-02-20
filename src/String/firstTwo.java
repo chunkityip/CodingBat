15280fe8b408942cfc061a820a8ac000d13bb11f
chunkityip <alex0917lfo@hotmail.com>
2023-02-20 16:47:10 +0800

    Adding firstTwo class in String

---
+++ src/String/firstTwo.java
@@ -0,0 +1,25 @@
+package String;
+
+public class firstTwo {
+    /*
+    Given a string, return the string made of its first two chars,
+    so the String "Hello" yields "He". If the string is shorter than length 2,
+    return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "".
+    Note that str.length() returns the length of a string.
+
+        firstTwo("Hello") → "He"
+        firstTwo("abcdefg") → "ab"
+        firstTwo("ab") → "ab"
+     */
+    public static void main (String[] args) {
+        System.out.println(firstTwo("Hello"));
+        System.out.println(firstTwo("abcdefg"));
+        System.out.println("ab");
+    }
+
+    public static String firstTwo(String str) {
+        if (str.length() <= 2) return str;
+        return str.substring(0 , 2);
+    }
+
+}
