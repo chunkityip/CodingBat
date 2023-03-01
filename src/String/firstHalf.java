857f8839c2c3a745d46202b56aff721f6a1465e7
chunkityip <alex0917lfo@hotmail.com>
2023-03-01 08:55:48 +0800

    Adding firstHalf class in HK on 3/1

---
+++ src/String/firstHalf.java
@@ -0,0 +1,23 @@
+package String;
+
+public class firstHalf {
+    /*
+    Given a string of even length, return the first half.
+    So the string "WooHoo" yields "Woo".
+
+
+    firstHalf("WooHoo") → "Woo"
+    firstHalf("HelloThere") → "Hello"
+    firstHalf("abcdef") → "abc"
+     */
+    public static void main (String [] args) {
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
+    }
+
+    public static String firstHalf(String str) {
+        int length = str.length() / 2;
+        return str.substring(0 , length);
+    }
+}
