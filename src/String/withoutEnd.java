cdc5108d84b614b783534f17c4c103c9167d7f6f
chunkityip <alex0917lfo@hotmail.com>
2023-03-02 11:43:26 +0800

    Adding withoutEnd class in HK on 3/1

--- src/String/withoutEnd.java
+++ src/String/withoutEnd.java
@@ -9,6 +9,7 @@ public class withoutEnd {
         withoutEnd("Hello") → "ell"
         withoutEnd("java") → "av"
         withoutEnd("coding") → "odin"
+        withoutEnd("Hello") → "ell"
      */
 
     public static void main (String[] args) {
