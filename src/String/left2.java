905e0294c6254a84f1c568543562e7684c6abb10
chunkityip <alex0917lfo@hotmail.com>
2023-03-07 12:52:04 +0800

    Adding left2 class in HK on 3/7

---
+++ src/String/left2.java
@@ -0,0 +1,25 @@
+package String;
+
+public class left2 {
+
+    /*
+    Given a string, return a "rotated left 2" version where the
+    first 2 chars are moved to the end. The string length will be at least 2.
+
+        left2("Hello") → "lloHe"
+        left2("java") → "vaja"
+        left2("Hi") → "Hi"
+     */
+
+    public static void main (String[] args) {
+        System.out.println(left2("Hello"));
+        System.out.println(left2("java"));
+        System.out.println(left2("Hi"));
+    }
+
+    public static String left2(String str) {
+        int length = str.length();
+        if (length <= 2) return str;
+        return str.substring(2) + str.substring(0 , 2);
+    }
+}
