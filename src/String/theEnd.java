35de7a5fb2f87367c4fedbef2ccacfb0dda5039a
chunkityip <alex0917lfo@hotmail.com>
2023-03-07 12:55:45 +0800

    Adding theEnd class in HK on 3/7

---
+++ src/String/theEnd.java
@@ -0,0 +1,25 @@
+package String;
+
+public class theEnd {
+
+    /*
+    Given a string, return a string length 1 from its front,
+    unless front is false, in which case return a string length 1 from its back.
+    The string will be non-empty.
+
+        theEnd("Hello", true) → "H"
+        theEnd("Hello", false) → "o"
+        theEnd("oh", true) → "o"
+     */
+
+    public static void main(String[] args) {
+        System.out.println(theEnd("Hello", true));
+        System.out.println(theEnd("Hello", false) );
+        System.out.println(theEnd("oh", true) );
+    }
+
+    public static String theEnd(String str, boolean front) {
+        return (front) ? str.substring(0 , 1) : str.substring(str.length() - 1);
+    }
+
+}
