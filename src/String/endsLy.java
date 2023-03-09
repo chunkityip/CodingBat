2029672411f4fb0efaf81e80bb1819e0d0c6b838
chunkityip <alex0917lfo@hotmail.com>
2023-03-09 11:11:03 +0800

    Adding endsLy class in HK on 3/9

---
+++ src/String/endsLy.java
@@ -0,0 +1,22 @@
+package String;
+
+public class endsLy {
+    /*
+        Given a string, return true if it ends in "ly".
+
+        endsLy("oddly") → true
+        endsLy("y") → false
+        endsLy("oddy") → false
+     */
+    public static void main (String[] args) {
+        System.out.println(endsLy("oddly"));
+        System.out.println(endsLy("y"));
+        System.out.println(endsLy("oddy"));
+    }
+
+    public static boolean endsLy(String str) {
+        int length = str.length();
+        if (length < 2) return false;
+        return str.substring(str.length() -2).equals("ly");
+    }
+}
