2c40afb94f5465de17d7038258f4e9e346bbe0f5
chunkityip <alex0917lfo@hotmail.com>
2023-04-13 06:35:34 -0400

    Adding caughtSpeeding class

---
+++ src/logic1/caughtSpeeding.java
@@ -0,0 +1,33 @@
+package logic1;
+
+public class caughtSpeeding {
+
+    public static void main (String[] args) {
+        System.out.println(caughtSpeeding(60 , false));
+        System.out.println(caughtSpeeding(65 , false));
+        System.out.println(caughtSpeeding(65 , true));
+    }
+
+    public static int caughtSpeeding(int speed, boolean isBirthday) {
+      /*
+
+      speed <= 60 , return 0
+      speed >= 61 && speed <= 80 return 1
+      speed >= 81 , return 2
+
+      isBirthday type , is (isBirthday) , speed -= 5
+
+      if (isBirthday) speed -= 5
+
+      speed <= 60 , return 0
+      speed >= 61 && speed <= 80 return 1
+      speed >= 81 , return 2
+
+      */
+        if (isBirthday) speed -= 5;
+        if (speed <= 60) return 0;
+        if (speed >= 61 && speed <= 80) return 1;
+
+        return 2;
+    }
+}
