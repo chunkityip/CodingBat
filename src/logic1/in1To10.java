c74b7ae7a63b80d6065857182b19a8baa5123d60
chunkityip <alex0917lfo@hotmail.com>
2023-04-15 07:06:34 -0400

    Adding in1To10 class

---
+++ src/logic1/in1To10.java
@@ -0,0 +1,25 @@
+package logic1;
+
+public class in1To10 {
+
+    public static void main (String [] args) {
+        System.out.println(in1To10(5, false));
+        System.out.println(in1To10(11, false));
+        System.out.println(in1To10(11, true) );
+    }
+
+    public static boolean in1To10(int n, boolean outsideMode) {
+  /*
+  Situation:
+  1. outsideMode is true
+  2. n >= 1 && n <= 10
+  */
+
+        if (outsideMode) {
+            return n <= 1 || n >= 10;
+        }
+
+        return  n >= 1 && n <= 10;
+    }
+
+}
