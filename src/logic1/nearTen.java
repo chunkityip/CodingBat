d2989641b1519040d9285a3e72d509c3f9a9132b
chunkityip <alex0917lfo@hotmail.com>
2023-04-16 10:54:23 -0400

    Adding nearThen

---
+++ src/logic1/nearTen.java
@@ -0,0 +1,15 @@
+package logic1;
+
+public class nearTen {
+
+    public static void main(String[] args) {
+        System.out.println(nearTen(12));
+        System.out.println(nearTen(17));
+        System.out.println(nearTen(19));
+    }
+
+    public static boolean nearTen(int num) {
+
+        return (num % 10 <= 2 || num % 10 >= 8);
+    }
+}
