9d4e015cbb90ab0588ee2b8477284c24ea887276
chunkityip <alex0917lfo@hotmail.com>
2023-04-17 11:59:37 -0400

    Adding fizzString class

---
+++ src/logic1/fizzString.java
@@ -0,0 +1,22 @@
+package logic1;
+
+public class fizzString {
+
+    public static void main (String [] args) {
+        System.out.println(fizzString("fig"));
+        System.out.println(fizzString("dib"));
+        System.out.println(fizzString("fib"));
+    }
+
+    public static String fizzString(String str) {
+
+        boolean front = str.startsWith("f");
+        boolean back = str.endsWith("b");
+
+        if(front && back) return "FizzBuzz";
+        if(front) return "Fizz";
+        if(back) return "Buzz";
+
+        return str;
+    }
+}
