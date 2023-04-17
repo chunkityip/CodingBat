973a6d541313d6a6ed25da69a6eb7289ea566f1c
chunkityip <alex0917lfo@hotmail.com>
2023-04-17 12:03:09 -0400

    Adding twoAsOne class

---
+++ src/logic1/twoAsOne.java
@@ -0,0 +1,22 @@
+package logic1;
+
+public class twoAsOne {
+
+    public static void main (String [] args) {
+        System.out.println(twoAsOne(1, 2, 3));
+        System.out.println(twoAsOne(3, 1, 2));
+        System.out.println(twoAsOne(3, 2, 2));
+    }
+
+    public static boolean twoAsOne(int a, int b, int c) {
+
+  /*
+  3 situations:
+  1. a + b = c
+  2. a + c = b
+  3. b + c = a;
+
+  */
+        return a + b == c || a + c == b || b + c == a;
+    }
+}
