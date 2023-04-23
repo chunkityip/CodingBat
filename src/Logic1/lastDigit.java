550a7bac8c5cba38b3556d63e202f3e4d66614b0
chunkityip <alex0917lfo@hotmail.com>
2023-04-23 10:46:12 -0400

    Adding lastDigit

---
+++ src/Logic1/lastDigit.java
@@ -0,0 +1,15 @@
+package Logic1;
+
+public class lastDigit {
+
+    public static void main (String [] args) {
+        System.out.println(lastDigit(23, 19, 13));
+        System.out.println(lastDigit(23, 19, 12);
+        System.out.println(lastDigit(23, 19, 3));
+    }
+
+    public static boolean lastDigit(int a, int b, int c) {
+
+        return a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10;
+    }
+}
