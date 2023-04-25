fd448b0d99e19c4db97f10cdc7c109302f0c427c
chunkityip <alex0917lfo@hotmail.com>
2023-04-25 06:23:03 -0400

    Adding catDog class

---
+++ src/String2/catDog.java
@@ -0,0 +1,23 @@
+package String2;
+
+public class catDog {
+
+    public static void main (String[] args) {
+        System.out.println(catDog("catdog"));
+        System.out.println(catDog("catcat"));
+        System.out.println(catDog("1cat1cadodog"));
+    }
+
+    public static boolean catDog(String str) {
+
+        int catCount = 0;
+        int dogCount = 0;
+
+        for (int i = 0; i < str.length() - 2; i++) {
+            if (str.substring(i , i + 3).equals("cat")) catCount++;
+            if (str.substring(i , i + 3).equals("dog")) dogCount++;
+        }
+
+        return catCount == dogCount;
+    }
+}
