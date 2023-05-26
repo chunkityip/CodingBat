e55ed9533a4797d93e4a2e35e74fd0fbf292efab
chunkityip <alex0917lfo@hotmail.com>
2023-05-26 13:42:24 -0400

    Adding new way to do countCode

--- src/String2/countCode.java
+++ src/String2/countCode.java
@@ -16,4 +16,15 @@ public class countCode {
         }
         return count;
     }
+
+    /*
+    Another way to doing so :
+    int count = 0;
+      for (int i = 0; i < str.length() - 3; i++) {
+        if (str.substring(i, i + 2).equals("co") && str.charAt(i + 3) == 'e') {
+          count++;
+    }
+  }
+  return count;
+     */
 }
