74fff7615a2b3171dcc32466629796d91cbc0f4c
chunkityip <alex0917lfo@hotmail.com>
2023-06-12 11:25:34 -0400

    Changing repeatSeparator answer

--- src/String2/repeatSeparator.java
+++ src/String2/repeatSeparator.java
@@ -9,16 +9,21 @@ public class repeatSeparator {
     }
 
     public static String repeatSeparator(String word, String sep, int count) {
-        if (count == 0) {
-            return "";
-        }
+        if (count == 1) return word;
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= count; i++) {
+        for (int i = 0; i < count; i++) {
             sb.append(word);
-            if (i != count) {
-                sb.append(sep);
-            }
+
+            /*
+            If the i is less than count - 1 , we are adding sep , else , skip it
+            Ex: i = 0 , i is less than 3 - 1 , add "X"
+            i = 1 , i is less than 3 - 1 , add "X"
+            i = 2 , i isn't less than 3 - 1 , we are not adding "X"
+             */
+            if (i < count - 1) sb.append(sep);
         }
+
         return sb.toString();
     }
 }
